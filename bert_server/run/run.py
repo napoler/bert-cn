968b54ae01c05fb6f92bf9ce3d4ad42e067051c8
Terry chan <napoler2008@gmail.com>
2019-05-21 02:35:43 +0800

    添加批量训练

---
+++ bert_server/run/run.py
@@ -0,0 +1,35 @@
+import subprocess
+import Terry_toolkit as tkit
+
+
+model_path ="/home/terry/pan/github/bert/model/"
+corpus_path ="/home/terry/pan/github/bert/book/"
+training_path="/home/terry/pan/github/bert/training"
+epochs='3'
+max_seq_len ="64"
+
+tfile=tkit.File()
+corpu_list= tfile.file_List(corpus_path,'txt')
+
+for item in corpu_list:
+  print("开始处理: "+item)
+
+
+
+  cmd = "python3 pregenerate_training_data.py --train_corpus "+item+"  --bert_model "+model_path+"last/ --do_lower_case --output_dir /home/terry/pan/github/bert/training/ --epochs_to_generate "+epochs+" --max_seq_len "+max_seq_len+"
+
+  print(subprocess.call(cmd, shell=True))
+
+
+  cmd_run="python3 finetune_on_pregenerated.py --pregenerated_data "+training_path+" --bert_model "+model_path+"last/ --do_lower_case --output_dir "+model_path+"temp/ --epochs "+epochs+" --no_cuda"
+
+
+  print(subprocess.call(cmd_run, shell=True))
+
+
+
+
+  cmd_mv="mv "+model_path+"temp/* "+model_path+"last/"
+
+
+  print(subprocess.call(cmd_mv, shell=True))
